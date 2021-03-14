366e56110fa1ad7db1677b06d1155aae7c10cd05
Jairo <jairo.moutinho@edu.ufes.br>
2021-03-14 19:41:17 -0300

    :sparkles: função para calcular o peso de todas as arestas do grafo de pontos

--- headers/mathShit.h
+++ headers/mathShit.h
@@ -1,3 +1,14 @@
+#ifndef _MATHSHIT_h_
+#define _MATHSHIT_h_
+
+
 #include <math.h>
+#include "../headers/ponto.h"
+
 
 double euclidDist(double *vetor1, double *vetor2, int dimensao);
+Aresta* wtArestas(Ponto* pontos, int dimensaoL, int dimensaoC, int nArestas);
+
+
+
+#endif
--- src/mathShit.c
+++ src/mathShit.c
@@ -1,16 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../headers/mathShit.h"
+
 
 double euclidDist(double *vetor1, double *vetor2, int dimensao)
 {
     double dist = 0;
     for(int i = 0; i < dimensao; i++)
     {
-        printf("O valor do vator é %lf\n",vetor1[i]);
         dist += pow((vetor1[i] - vetor2[i]), 2);
-        printf("A variávle dist atualmente tem o valor %lf\n", dist);
     }
 
     dist = sqrt(dist);
 
     return dist;
 }
+
+
+Aresta* wtArestas(Ponto* pontos, int dimensaoL, int dimensaoC, int nArestas)
+{
+    Aresta *grafo = malloc(sizeof(Aresta) * nArestas);
+    int cont = 0;
+    for(int i = 0; i < dimensaoL - 1; i++)
+    {
+        for(int j = i+1; j < dimensaoL; j++)
+        {
+            double distAB = euclidDist(pontos[i].components, pontos[j].components, dimensaoC);
+            grafo[cont].pontoA = &pontos[i];
+            grafo[cont].pontoB = &pontos[j];
+            grafo[cont].distAB = distAB;
+            cont++;
+        }
+    }
+
+    return grafo;
+}
+
